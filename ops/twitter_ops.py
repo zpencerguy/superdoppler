f44a64bbaf29529b2f58e9750a1e5b7335c31795
spencerguy <spencer.guy@cryptoslam.io>
2023-01-06 21:37:27 -0700

    twitter bot

--- ops/twitter_ops.py
+++ ops/twitter_ops.py
@@ -11,10 +11,11 @@ def tweet_prediction(context):
     template = context.op_config['template']
     for i in data:
         tweet_text = template.format(**i)
-        if i['image']:
-            try:
-                tweet = twitter.post_tweet_with_uri_image(tweet_text, i['image'])
-            except:
-                tweet = twitter.post_tweet(tweet_text)
-        else:
-            tweet = twitter.post_tweet(tweet_text)
+        # if i['image']:
+        #     try:
+        #         tweet = twitter.post_tweet_with_uri_image(tweet_text, i['image'])
+        #     except:
+        #         tweet = twitter.post_tweet(tweet_text)
+        # else:
+        #     tweet = twitter.post_tweet(tweet_text)
+        tweet = twitter.post_tweet(tweet_text)
